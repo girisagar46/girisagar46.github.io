3e0a91c4fd38b5b54315ac6fed120be03659cb48
Sagar Giri <girisagar46@gmail.com>
2017-01-27 23:29:06 +0545

    feed url fixed

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,8 @@ from pelicanconf import *
 SITEURL = 'https://girisagar46.github.io'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = SITEURL+'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = SITEURL+'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
