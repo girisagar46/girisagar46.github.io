95ca190a02c263a72e50ae73df84c5742ffeece5
Sagar Giri <girisagar46@gmail.com>
2017-01-28 16:39:48 +0545

    adsense added

--- pelicanconf.py
+++ pelicanconf.py
@@ -88,8 +88,16 @@ FEED_USE_SUMMARY = True
 
 GOOGLE_ANALYTICS = 'UA-73000395-1'
 DISQUS_SITENAME = 'girisagar46-github-io'
-ADSENSE = {
-    'adClientId' : 'ca-pub-6204331251488075'
+GOOGLE_ADSENSE = {
+    'ca_id' : 'ca-pub-6204331251488075',
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
 }
 
 # Formatting for URLS
