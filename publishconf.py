3327c76e41dbcacefec3ae55f33131fbdb594c62
gsagar <gsagar@insightworkshop.io>
2019-02-08 17:55:32 +0545

    restore publishconf

--- publishconf.py
+++ publishconf.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-
-import os
-import sys
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
+
+import os
+import sys
 sys.path.append(os.curdir)
+from pelicanconf import *
 
 SITEURL = 'https://girisagar46.github.io'
 RELATIVE_URLS = False
